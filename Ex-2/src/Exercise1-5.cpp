4d0972e5ecd085caa0f1792e170b408d5cd2b3db
Sami <samiabdu.11377@gmail.com>
2022-09-14 19:41:44 -0400

    Task One

---
+++ Ex-2/src/Exercise1-5.cpp
@@ -0,0 +1,11 @@
+  #include <iostream>
+     int main(){
+     	int opd1=0;
+	int opd2=0;
+	std::cin>>opd1>>opd2;
+	std::cout<< " The sum of " << opd1;
+	std::cout << " and " << opd2;
+	std::cout << " is " << opd1+opd2;
+	return 0;
+     
+     }
